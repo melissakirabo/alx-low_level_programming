c028c27ac541f11508d9ad129bc7d6a1984f5d80
Melissa Kirabo <kirabo777@gmail.com>
2021-06-21 18:05:14 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,8 +6,8 @@ int main()
     int x;
     for(x = '0'; x <= '9'; x++)
     {
-        putchar(x);
-        putchar(', ');
+        putchar(x + ', ');
+        
     }
     putchar('\n');
     return 0;
