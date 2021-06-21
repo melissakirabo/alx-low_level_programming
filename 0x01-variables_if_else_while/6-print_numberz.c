e956398d52d5ef960320fdd2af99250127c17d08
Melissa Kirabo <kirabo777@gmail.com>
2021-06-21 18:10:25 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,27 @@
-#include <stdio.h>
-#include <ctype.h>
+/*
+ * File: 6-print_base16.c
+ * Auth: Melissa Kirabo
+ */
 
-int main()
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-    int x;     
-    for(x = '0'; x < '10'; x++)
-    {         
-        putchar(x); 
-    }
-    putchar('\n');
-    return 0;
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
