f38abab08b471830589dfc4649a9e553bf2cacde
Melissa Kirabo <kirabo777@gmail.com>
2021-06-21 15:56:24 +0300

    Add files via upload

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    int x;     
+    for(x = 'A'; x <= 'Z'; x++)
+    {
+        putchar(tolower(x));   
+        
+        if (x = 'E' || X = 'Q') 
+        {
+        }
+    }
+    putchar('\n');
+    return 0;
+}
